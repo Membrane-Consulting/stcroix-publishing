4e0e4e564e4ef0b733bf4dae9f27176b656960ce
GHOST <ghostdevbusiness@gmail.com>
2022-01-29 23:38:01 +0000

    fix: email requests

--- packages/web/src/routes/api/request.ts
+++ packages/web/src/routes/api/request.ts
@@ -1,49 +1,43 @@
-import mail from '@sendgrid/mail'
 import type { MessageContent } from '$lib/types/email'
+import type { RequestEvent } from '@sveltejs/kit'
+import mail from '@sendgrid/mail'
 
-/** @type {import('@sveltejs/kit').RequestHandler} */
+export async function post({ request }: RequestEvent) {
+  const data = await request.formData();
 
-export async function post({ body }) {
-
-  await mail.setApiKey(process.env["SENDGRID_API_KEY"])
+  mail.setApiKey(process.env["SENDGRID_API_KEY"])
 
   const address = import.meta.env.VITE_EMAIL_ADDRESS.toString()
 
-  const msg:MessageContent = {
+  const msg: MessageContent = {
     to: address,
-    from: body.get("email"),
-    subject: `New Account Request from ${body.get("name")} at ${body.get("institution")}`,
-    text: `New Account Request from ${body.get("name")} - ${body.get("role")} at ${body.get("institution")} Email: ${body.get("email")}`,
+    from: address,
+    subject: `New Account Request from ${data.get("name")} at ${data.get("institution")}`,
+    text: `New Account Request from ${data.get("name")} - ${data.get("role")} at ${data.get("institution")} Email: ${data.get("email")}`,
     html: `
-      <p>New Account Request from ${body.get("name")}</p>
-      <p>${body.get("role")} at ${body.get("institution")}</p>
-      <p>Email: ${body.get("email")}</p>
+      <p>New Account Request from ${data.get("name")}</p>
+      <p>${data.get("role")} at ${data.get("institution")}</p>
+      <p>Email: ${data.get("email")}</p>
     `,
   }
 
-  mail
-    .send(msg)
-    .then(res => {
-      return {
-        status: 200,
-        body: res,
-      };
-    })
-    .catch((error) => {
-      console.error(error)
-      return {
-        status: 500,
-        headers: {
-          Location: '/request?status=error'
-        },
-        body: { error },
-      }
-    })
+  try {
+    await mail.send(msg);
 
-	return {
-    status: 302,
-    headers: {
-      Location: '/request?status=success'
+    return {
+      status: 302,
+      headers: {
+        Location: '/request?status=success'
+      }
+    };
+  }
+  catch (error) {
+    return {
+      status: 500,
+      headers: {
+        Location: '/request?status=error'
+      },
+      body: { error },
     }
-  };
+  }
 }
