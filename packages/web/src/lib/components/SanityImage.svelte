979e5b66042ee4d2facbf19936721635f6212f35
stordahl <jacob@stordahl.dev>
2022-02-05 18:27:10 -0600

    Fixes load bug in SanityImage

--- packages/web/src/lib/components/SanityImage.svelte
+++ packages/web/src/lib/components/SanityImage.svelte
@@ -1,32 +1,50 @@
 <script lang=ts>
   import {urlFor} from '$lib/sanityClient'
   import type { Image } from '@sanity/types'
+  import { browser } from '$app/env'
+  import { onMount } from 'svelte'
+
+  // interface AccessibleImage extends Image {
+  //   alt: string
+  // }
+
   export let image:Image
-  export let maxWidth = 1200
-  export let alt = undefined
-  export let style = undefined
-  // Example image document ID: image-cc93b69600f5cd1abce97fd0d4aa71793dbbba76-1350x900-png
-  // Structure: image-${storedImgId}-${dimensions}-${format}
-  // If we split it by "-", the 3rd element are the dimensions (1350x900)
+  export let maxWidth:number = 1200
+  export let alt:string = undefined
+  export let style:string = undefined
+
   $: dimensions = image?.asset?._ref?.split('-')[2]
-  // If we split dimensions by "x", we get the width (1350) and height (900)
   $: [width, height] = dimensions.split('x').map(Number)
   $: aspectRatio = width / height
-  // Once loaded, the image will transition to full opacity
+
+  let imageRef
+
   $: loaded = false;
+
+  onMount(() => {
+    imageRef.onload = () => {
+      loaded = true
+    }
+  }) 
 </script>
-{#if image}
+{#if browser && image}
   <img
     loading="lazy"
     src={urlFor(image).width(maxWidth).fit('fillmax').url()}
-    alt={alt || image.alt || ''}
-    style={`aspect-ratio: ${aspectRatio}; opacity: ${loaded ? 1 : 0}; transition: .1s opacity; ${style}`}
-    on:load={() => (loaded = true)}
+    alt={alt|| ''}
+    class:loaded
+    bind:this={imageRef}
+    style="aspect-ratio: {aspectRatio}; {style ? style : ''}"
   />
 {/if}
 
-<style>
+<style> 
   img {
     border-radius: 40px;
-  }  
+    opacity: 0;
+    transition: opacity 100ms ease-out;
+  }
+  img.loaded {
+    opacity: 1;
+  }
 </style>
