14367f23f8297ca7e095530f5c351538c2df8d78
stordahl <jacob@stordahl.dev>
2022-02-11 12:54:48 -0600

    Trying adapter static

--- packages/web/svelte.config.js
+++ packages/web/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
